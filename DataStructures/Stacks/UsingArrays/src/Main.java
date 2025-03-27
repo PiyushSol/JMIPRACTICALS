54ece42810cb31bc650e60685ea6e5faef0974eb
Piyush Solanki <104305256+PiyushSol@users.noreply.github.com>
2025-03-27 12:58:56 +0530

    Added Evaluation of Postfix expression method.

--- DataStructures/Stacks/UsingArrays/src/Main.java
+++ DataStructures/Stacks/UsingArrays/src/Main.java
@@ -1,31 +1,33 @@
+package UsingArrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("------Stack with Arrays-------");
-        System.out.print("Enter the size of your stack : ");
-        int n= sc.nextInt();
-        Stack s  = new Stack(n);
+        System.out.print("Enter the size of your stack: ");
+        int n = sc.nextInt();
+        Stack s = new Stack(n);
         menu(s);
         sc.close();
     }
 
-    public static void menu(Stack s){
+    public static void menu(Stack s) {
         Scanner sc = new Scanner(System.in);
         int choice;
-        while(true){
+        while (true) {
             System.out.println("-".repeat(10));
             System.out.println("1. Push Element");
             System.out.println("2. Pop Element");
             System.out.println("3. Peek Element");
-            System.out.println("4. Exit");
+            System.out.println("4. Evaluate Postfix Expression");
+            System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
 
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    System.out.print("Enter the element : ");
+                    System.out.print("Enter the element: ");
                     int el = sc.nextInt();
                     try {
                         s.push(el);
@@ -35,7 +37,8 @@ public class Main {
                     break;
                 case 2:
                     try {
-                        s.pop();
+                        int d = s.pop();
+                        System.out.println(d + " popped out from the stack.");
                     } catch (StackEmptyException e) {
                         System.out.println(e.getMessage());
                     }
@@ -48,6 +51,19 @@ public class Main {
                     }
                     break;
                 case 4:
+                 
+                    System.out.println("Enter your postfix expression : ");
+                    sc.nextLine(); 
+                    String exp = sc.nextLine();
+
+                    if (exp.trim().isEmpty()) {
+                        System.out.println("Invalid input: empty postfix expression.");
+                    } else {
+                        int res = s.evaluate(exp);
+                        System.out.println("The evaluation is: " + res);
+                    }
+                    break;
+                case 5:
                     System.out.println("Exiting......");
                     sc.close();
                     System.exit(0);
