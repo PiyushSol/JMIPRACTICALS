30e96eeeacf7370ae684e1edb123a0aacbd20db9
Piyush Solanki <104305256+PiyushSol@users.noreply.github.com>
2025-07-28 13:45:40 +0530

    Permutations - Recursive and Iterative

--- ADA/Permutation.java
+++ ADA/Permutation.java
@@ -1,7 +1,59 @@
+import java.util.Arrays;
+
 public class Permutation {
     public static void main(String[] args) {
-        String a[] = new String[]{"A","B","C"};
-        permutations(a, 0, a.length-1);
+        String a[] = new String[] { "F","D","E","A", "B", "C" };
+        int n = a.length;
+        long start = System.currentTimeMillis();
+        permutations(a, 0, n-1);
+        long end = System.currentTimeMillis();
+        System.out.println("Recursive Permutations time : " + (end - start) + " ms.");
+        
+        System.out.println("-".repeat(60));
+        
+        long start1 = System.currentTimeMillis();
+        printAllPermutations(a, n);
+           long end1 = System.currentTimeMillis();
+        System.out.println("Iterative Permutations time : " + (end1 - start1) + " ms.");
+    }
+
+    //Iterative Next Permutations
+    public static boolean permutations_iterative(String a[], int n) {
+        int i = n - 2;
+        while (i >= 0 && a[i].compareTo(a[i + 1]) > 0) {
+            i = i - 1;
+        }
+        if (i == -1) {
+            return false;
+        }
+        int j = n - 1;
+        while (a[j].compareTo(a[i]) < 0) {
+            j = j - 1;
+        }
+
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        reverseArray(a, i + 1, n - 1);
+        return true;
+    }
+
+    public static void printAllPermutations(String a[], int n) {
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        while (permutations_iterative(a, n) == true) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
+    public static void reverseArray(String a[], int s, int e) {
+        while (s < e) {
+            String temp = a[s];
+            a[s] = a[e];
+            a[e] = temp;
+            s++;
+            e--;
+        }
     }
     
     public static void permutations(String a[], int k, int n) {
