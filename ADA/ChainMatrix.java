48900f16079e3167e8d089bd8077e3dce4b73c05
Piyush Solanki <104305256+PiyushSol@users.noreply.github.com>
2025-08-21 11:00:57 +0530

    Updated Chain Matrix

--- ADA/ChainMatrix.java
+++ ADA/ChainMatrix.java
@@ -1,31 +1,46 @@
 public class ChainMatrix {
     public static void main(String[] args){
-        int n =3;
-        int[] d = {10,20,5,30};
-        int[][] P = new int[n+1][n+1];
-        minMul(d,P,n);
-        printOrder(1,n,P,n);
-
+        int n =4;
+        int[] d = {5, 2, 3, 4, 6};
+        minMul(d,n);
     }
 
-    public static void minMul(int []d , int P[][] , int n){
-        int M[][] = new int[n+1][n+1];
-        for(int i=1;i<n;i++){
+    public static void minMul(int []d, int n){
+        int M[][] = new int[n][n];
+        int[][] P = new int[n][n];
+        for(int i=0;i<n;i++){
             M[i][i] =0;
         }
-        for(int diagonal = 1 ; diagonal<=n;diagonal++){
-            for(int i=1; i<=n-diagonal;i++){
-                int j = i+diagonal;
-                M[i][j] = M[i][j]+M[i+1][j]+d[i-1]*d[i]*d[j];
-                P[i][j]=i;
-                for(int k=i+1;k<=j-1;k++){
-                    if((M[i][k]+M[k+1][j]+d[i-1]*d[k]*d[j])<M[i][j]){
-                        M[i][j]=M[i][k]+M[k+1][j]+d[i-1]*d[k]*d[j];
-                        P[i][j]=k;
+        for (int diagonal = 1; diagonal < n; diagonal++) {
+            for (int i = 0; i <= n - diagonal-1; i++) {
+                int j = i + diagonal;
+                int k = i;
+                M[i][j] = M[i][k] + M[k + 1][j] + d[i] * d[k+1] * d[j+1];
+                P[i][j] = k;
+                for (k = i + 1; k <= j - 1; k++) {
+                    if ((M[i][k] + M[k + 1][j] + d[i] * d[k+1] * d[j+1]) < M[i][j]) {
+                        M[i][j] = M[i][k] + M[k + 1][j] + d[i] * d[k+1] * d[j+1];
+                        P[i][j] = k;
                     }
                 }
             }
         }
+        
+        System.out.println("Printing the matrix M for chain matrix : ");
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                System.out.print(M[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Printing the matrix P for chain matrix : ");
+        for (int i = 0; i < P.length; i++) {
+            for (int j = 0; j < P.length; j++) {
+                System.out.print(P[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        printOrder(0, n-1, P, n);
     }
 
     public static void printOrder(int i, int j, int[][]p,int n){
